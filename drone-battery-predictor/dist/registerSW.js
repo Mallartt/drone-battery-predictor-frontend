22b852d6d785e93f3961e316e4c27f9a0f964ecb
Mallartt <mallartt@gmail.com>
2025-10-20 23:28:55 +0300

    add dist for gh-pages deploy

---
+++ drone-battery-predictor/dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/drone-battery-predictor-frontend/sw.js', { scope: '/drone-battery-predictor-frontend/' })})}
